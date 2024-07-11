1c1f9233cf536210754f62eb6f294edf567f00ed
dilane3 <komboudilane125@gmail.com>
2024-07-11 12:24:40 +0100

    chore: upgrade rasengan to 1.0.0-beta.37

--- templates/saas/model1/rasengan.config.js
+++ templates/saas/model1/rasengan.config.js
@@ -2,28 +2,4 @@ import { defineConfig } from "rasengan";
 
 export default defineConfig({
   reactStrictMode: true,
-
-  // Define aliases
-  vite: {
-    resolve: {
-      alias: [
-        {
-          find: "@app",
-          replacement: "./src/app",
-        },
-        {
-          find: "@components",
-          replacement: "./src/components",
-        },
-        {
-          find: "@assets",
-          replacement: "./src/assets",
-        },
-        {
-          find: "@styles",
-          replacement: "./src/styles",
-        },
-      ],
-    },
-  },
 });
