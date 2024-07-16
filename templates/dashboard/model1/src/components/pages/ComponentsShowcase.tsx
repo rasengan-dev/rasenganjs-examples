93214ef269adfd440b5507039e72833979ab9b83
konnofuente <konomelifuente@gmail.com>
2024-07-16 16:31:43 +0100

    refactor(app): #7 updated varient

--- templates/dashboard/model1/src/components/pages/ComponentsShowcase.tsx
+++ templates/dashboard/model1/src/components/pages/ComponentsShowcase.tsx
@@ -5,16 +5,22 @@ import { Button } from "../atoms/button";
 const ComponentsShowcase: PageComponent = () => {
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold mb-8 text-center">Components Showcase</h1>
+      <h1 className="text-6xl font-bold mb-8 text-center">Components Showcase</h1>
       
       <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4">Atoms</h2>
-        <div className="flex space-x-4" >
+        <h2 className="text-4xl font-semibold mb-4">Atoms</h2>
 
-        <Button variant={'green'} >Rasengan</Button>
-        <Button variant={'greenSecondary'} >Rasengan</Button>
-        <Button variant={'green'} >Rasengan</Button>
-        <Button variant={'green'} >Rasengan</Button>
+
+        <h3 className="text-xl font-semibold mb-2" >Buttons</h3>
+        <div className="flex space-x-4" >
+        <Button variant={'primary'} >primary</Button>
+        <Button variant={'primarySecondary'} >primarySecondary</Button>
+        <Button variant={'red'} >red</Button>
+        <Button variant={'redSecondary'} >redSecondary</Button>
+        <Button variant={'green'} >green</Button>
+        <Button variant={'greenSecondary'} >greenSecondary</Button>
+        <Button variant={'warning'} >warning</Button>
+        <Button variant={'warningSecondary'} >warningSecondary</Button>
         </div>
       </section>
 
