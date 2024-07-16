52d6320909dcc4e9e4507c58abfc3d43cb573be1
dilane3 <komboudilane125@gmail.com>
2024-07-16 18:52:37 +0100

    feat: Adding Input component

---
+++ templates/dashboard/model1/src/components/atoms/Input.tsx
@@ -0,0 +1,32 @@
+import clsx from "clsx";
+import { ComponentProps } from "react";
+
+interface InputProps extends Omit<ComponentProps<"input">, "size"> {
+	children?: React.ReactNode;
+	size?: "small" | "medium" | "large";
+}
+
+export default function Input({
+	children,
+	size = "small",
+	className,
+	...rest
+}: InputProps) {
+	return (
+		<div className='relative w-auto h-auto'>
+			<input
+				{...rest}
+				className={clsx(
+					"w-full px-4 py-4 rounded-md bg-transparent border-[1px] border-border outline-primary transition-all",
+					children ? "pl-12" : "",
+          size === "small" ? "h-10" : size === "medium" ? "h-14" : "h-18",
+					className
+				)}
+			/>
+
+			<div className='absolute inset-y-0 left-4 top-1/2 -translate-y-1/2 flex items-center justify-center'>
+				{children}
+			</div>
+		</div>
+	);
+}
