4e76128d66634695daf5e4a48d7c0e36ed7b0ddb
Assaf1er <assaffart@gmail.com>
2024-08-01 00:58:54 +0100

    completed about page

--- templates/saas/model1/src/components/molecules/TestimonialCArd.tsx
+++ templates/saas/model1/src/components/molecules/TestimonialCArd.tsx
@@ -1,10 +1,10 @@
-import { ComponentProps } from "react";
-import { useTheme } from "@rasenganjs/theme";
-import { StarIcon } from "hugeicons-react";
-import Typography from "../atoms/Typography";
+import { ComponentProps } from 'react';
+import { useTheme } from '@rasenganjs/theme';
+import { StarIcon } from 'hugeicons-react';
+import Typography from '../atoms/Typography';
 interface TestimonialProps {
   children?: React.ReactNode;
-  className?: ComponentProps<"div">["className"];
+  className?: ComponentProps<'div'>['className'];
   testimonial: string;
   author: string;
   profile: string;
@@ -21,32 +21,30 @@ export default function TestimonialCard({
   const { isDark } = useTheme();
 
   return (
-    
-      <div
-        className={` w-full flex flex-col items-center justify-between md:w-[280px]  p-6 lg:p-7 h-[250px] lg:h-[300px] rounded-2xl hover:scale-105 ease-in-out duration-[400ms]   ${
-          isDark
-            ? `bg-rasengan-dark-background text-rasengan-dark-text shadow-rasengan-dark-shadow 
+    <div
+      className={` w-full flex flex-col items-center justify-between md:w-[280px]  p-6 lg:p-7 h-[250px] lg:h-[300px] rounded-2xl hover:scale-105 ease-in-out duration-[400ms]   ${
+        isDark
+          ? `bg-rasengan-dark-background text-rasengan-dark-text shadow-rasengan-dark-shadow 
                  border-[1px] border-rasengan-dark-border hover:shadow-2xl
               `
-            : `bg-rasengan-light-background text-rasengan-light-text shadow-rasengan-light-shadow 
+          : `bg-rasengan-light-background text-rasengan-light-text shadow-rasengan-light-shadow 
                  shadow-lg hover:shadow-2xl
               }`
-        }  ${className}`}
-      >
-        <Typography
-          text={`"${testimonial}"`}
-          className="font-bold text-[18px] lg:text-xl text-center h-[80px] lg:h-[100px]  "
-        />
-        <ul className="flex gap-2 my-2 lg:my-4">
-          {Array.from({ length: ranking }, (_, index) => (
-            <li>
-              <StarIcon key={index} className="text-rasengan-light-primary" />
-            </li>
-          ))}
-        </ul>
-        <Typography text={author} className="font-bold text-lg " />
-        <Typography text={profile} className="text-sm " />
-      </div>
-   
+      }  ${className}`}
+    >
+      <Typography
+        text={`"${testimonial}"`}
+        className="font-bold text-[18px] lg:text-xl text-center h-[80px] lg:h-[100px]  "
+      />
+      <ul className="flex gap-2 my-2 lg:my-4">
+        {Array.from({ length: ranking }, (_, index) => (
+          <li key={index}>
+            <StarIcon key={index} className="text-rasengan-light-primary" />
+          </li>
+        ))}
+      </ul>
+      <Typography text={author} className="font-bold text-lg " />
+      <Typography text={profile} className="text-sm " />
+    </div>
   );
 }
