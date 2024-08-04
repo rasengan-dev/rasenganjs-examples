6817983de48024d1c8eb35b642c9acbb7cb00ddc
dilane3 <komboudilane125@gmail.com>
2024-08-04 16:15:39 +0100

    chore: upgrade rasengan to 1.0.0-beta.40

--- templates/saas/model1/api/index.js
+++ templates/saas/model1/api/index.js
@@ -53,31 +53,36 @@ import { fileTypeFromBuffer } from "file-type";
 // Create server for production only
 export default function handler(req, res) {
     return __awaiter(this, void 0, void 0, function () {
-        var url, host, appPath, filePath, file, err_1, segments, segmentsWithoutOrigin, _i, segments_1, segment, filePath, file, otherFile, result, mimeType, file, templateHtml, serverFilePath, bootstrapDirPath, entry, bootstrap, styles, render, staticRoutes, loadTemplateHtml, handler_1, fetchRequest, context, status_1, redirect, helmetContext, router, rendered, html, e_1;
+        var url, host, appPath, configPath, config, filePath, file, err_1, segments, segmentsWithoutOrigin, _i, segments_1, segment, filePath, file, otherFile, result, mimeType, file, templateHtml, serverFilePath, bootstrapDirPath, entry, bootstrap, styles, render, staticRoutes, loadTemplateHtml, handler_1, fetchRequest, context, status_1, redirect, helmetContext, router, rendered, html, e_1;
         var _a;
         return __generator(this, function (_b) {
             switch (_b.label) {
                 case 0:
-                    _b.trys.push([0, 15, , 16]);
+                    _b.trys.push([0, 18, , 19]);
                     url = req.url;
                     host = req.headers.host;
                     appPath = process.cwd();
-                    if (!(url === "/robots.txt")) return [3 /*break*/, 5];
-                    _b.label = 1;
+                    console.log({ env: process.cwd() });
+                    configPath = path.resolve(join(process.cwd() + "./../../", "rasengan.config.js"));
+                    return [4 /*yield*/, import(configPath)];
                 case 1:
-                    _b.trys.push([1, 4, , 5]);
+                    config = (_b.sent()).default;
+                    if (!(url === "/robots.txt")) return [3 /*break*/, 6];
+                    _b.label = 2;
+                case 2:
+                    _b.trys.push([2, 5, , 6]);
                     filePath = join(appPath, "dist/client/robots.txt");
                     return [4 /*yield*/, fs.access(path.resolve(filePath))];
-                case 2:
+                case 3:
                     _b.sent();
                     return [4 /*yield*/, fs.readFile(filePath, "utf-8")];
-                case 3:
+                case 4:
                     file = _b.sent();
                     return [2 /*return*/, res.send(file)];
-                case 4:
+                case 5:
                     err_1 = _b.sent();
                     return [2 /*return*/, res.send("\n          user-agent: *\n          disallow: /downloads/\n          disallow: /private/\n          allow: /\n          \n          user-agent: magicsearchbot\n          disallow: /uploads/\n        ")];
-                case 5:
+                case 6:
                     // ! Sitemap Fix
                     if (url === "/sitemap.xml") {
                         return [2 /*return*/, res.send(path.resolve(join(appPath, "dist/client/sitemap.xml")))];
@@ -86,7 +91,7 @@ export default function handler(req, res) {
                     if (url === "/manifest.json") {
                         return [2 /*return*/, res.send(path.resolve(join(appPath, "dist/client/manifest.json")))];
                     }
-                    if (!url.includes("/assets")) return [3 /*break*/, 9];
+                    if (!url.includes("/assets")) return [3 /*break*/, 10];
                     segments = url.split("/");
                     segmentsWithoutOrigin = __spreadArray([], segments, true);
                     for (_i = 0, segments_1 = segments; _i < segments_1.length; _i++) {
@@ -98,45 +103,51 @@ export default function handler(req, res) {
                     }
                     filePath = join(appPath, "dist/client", segmentsWithoutOrigin.join("/"));
                     return [4 /*yield*/, fs.readFile(filePath, "utf-8")];
-                case 6:
+                case 7:
                     file = _b.sent();
                     if (url.endsWith(".js") || url.endsWith(".css")) {
                         return [2 /*return*/, new Response(file, {
                                 headers: {
-                                    "Content-Type": url.endsWith(".js") ? "text/javascript" : "text/css",
+                                    "Content-Type": url.endsWith(".js")
+                                        ? "text/javascript"
+                                        : "text/css",
                                     "Cache-Control": "max-age=31536000",
                                 },
                             })];
                     }
                     return [4 /*yield*/, fs.readFile(filePath)];
-                case 7:
+                case 8:
                     otherFile = _b.sent();
                     return [4 /*yield*/, fileTypeFromBuffer(otherFile)];
-                case 8:
+                case 9:
                     result = _b.sent();
-                    mimeType = result ? result.mime : url.endsWith(".svg") ? "image/svg+xml" : "application/octet-stream";
+                    mimeType = result
+                        ? result.mime
+                        : url.endsWith(".svg")
+                            ? "image/svg+xml"
+                            : "application/octet-stream";
                     return [2 /*return*/, new Response(otherFile, {
                             headers: {
                                 "Content-Type": mimeType,
                                 "Cache-Control": "max-age=31536000",
                             },
                         })];
-                case 9:
-                    if (!(url.endsWith(".js") || url.endsWith(".css"))) return [3 /*break*/, 11];
-                    return [4 /*yield*/, fs.readFile(url, "utf-8")];
                 case 10:
+                    if (!(url.endsWith(".js") || url.endsWith(".css"))) return [3 /*break*/, 12];
+                    return [4 /*yield*/, fs.readFile(url, "utf-8")];
+                case 11:
                     file = _b.sent();
                     return [2 /*return*/, res
                             .status(200)
                             .setHeader("Content-Type", url.endsWith(".js") ? "text/javascript" : "text/css")
                             .setHeader("Cache-Control", "max-age=31536000")
                             .end(file)];
-                case 11:
+                case 12:
                     templateHtml = "";
                     serverFilePath = join(appPath, "dist/server/entry-server.js");
                     bootstrapDirPath = join(appPath, "dist/client/assets");
                     return [4 /*yield*/, import(serverFilePath)];
-                case 12:
+                case 13:
                     entry = _b.sent();
                     bootstrap = "/assets/" +
                         fsSync
@@ -150,7 +161,7 @@ export default function handler(req, res) {
                     handler_1 = createStaticHandler(staticRoutes);
                     fetchRequest = createFetchRequest(req, host);
                     return [4 /*yield*/, handler_1.query(fetchRequest)];
-                case 13:
+                case 14:
                     context = _b.sent();
                     status_1 = context.status;
                     if (status_1 === 302) {
@@ -160,8 +171,11 @@ export default function handler(req, res) {
                     }
                     helmetContext = {};
                     router = createStaticRouter(handler_1.dataRoutes, context);
-                    return [4 /*yield*/, render(router, context, helmetContext)];
-                case 14:
+                    if (!config.experimental.stream) return [3 /*break*/, 16];
+                    return [4 /*yield*/, render(router, context, helmetContext, bootstrap, styles, res)];
+                case 15: return [2 /*return*/, _b.sent()];
+                case 16: return [4 /*yield*/, render(router, context, helmetContext)];
+                case 17:
                     rendered = _b.sent();
                     // Load template html
                     if (!templateHtml) {
@@ -174,12 +188,12 @@ export default function handler(req, res) {
                             .setHeader("Content-Type", "text/html")
                             .setHeader("Cache-Control", "max-age=31536000")
                             .end(html)];
-                case 15:
+                case 18:
                     e_1 = _b.sent();
                     console.log(e_1.stack);
                     res.status(500).end(e_1.stack);
-                    return [3 /*break*/, 16];
-                case 16: return [2 /*return*/];
+                    return [3 /*break*/, 19];
+                case 19: return [2 /*return*/];
             }
         });
     });
