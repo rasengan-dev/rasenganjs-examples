978e7257c4bd58af80d23375afbd7690adfbb901
dilane3 <komboudilane125@gmail.com>
2024-12-18 11:14:49 +0100

    chore: Update rasengan to 1.0.0-beta.50

--- templates/saas/model1/api/index.js
+++ templates/saas/model1/api/index.js
@@ -8,8 +8,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -54,12 +54,11 @@ var RASENGAN_VERCEL_CONFIG = process.env.RASENGAN_VERCEL_CONFIG;
 // Create server for production only
 export default function handler(req, res) {
     return __awaiter(this, void 0, void 0, function () {
-        var url, host, appPath, config_1, configPath, config, filePath, file, err_1, segments, segmentsWithoutOrigin, _i, segments_1, segment, filePath, file, otherFile, result, mimeType, file, templateHtml, serverFilePath, bootstrapDirPath, entry, bootstrap, styles, render, staticRoutes, loadTemplateHtml, handler_1, fetchRequest, context, status_1, redirect, helmetContext, router, rendered, html, e_1;
-        var _a;
-        return __generator(this, function (_b) {
-            switch (_b.label) {
+        var url, host, appPath, config_1, configPath, config, filePath, file, err_1, segments, segmentsWithoutOrigin, _i, segments_1, segment, filePath, file, otherFile, result, mimeType, file, templateHtml, serverFilePath, bootstrapDirPath, entry, bootstrap, styles, render, staticRoutes, loadTemplateHtml, handler_1, fetchRequest, context, status_1, redirect, helmetContext, router, e_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
                 case 0:
-                    _b.trys.push([0, 18, , 19]);
+                    _a.trys.push([0, 16, , 17]);
                     url = req.url;
                     host = req.headers.host;
                     appPath = process.cwd();
@@ -72,21 +71,21 @@ export default function handler(req, res) {
                     configPath = path.resolve(join(appPath, "rasengan.config.js"));
                     return [4 /*yield*/, import(configPath)];
                 case 1:
-                    config = (_b.sent()).default;
+                    config = (_a.sent()).default;
                     if (!(url === "/robots.txt")) return [3 /*break*/, 6];
-                    _b.label = 2;
+                    _a.label = 2;
                 case 2:
-                    _b.trys.push([2, 5, , 6]);
+                    _a.trys.push([2, 5, , 6]);
                     filePath = join(appPath, "dist/client/robots.txt");
                     return [4 /*yield*/, fs.access(path.resolve(filePath))];
                 case 3:
-                    _b.sent();
+                    _a.sent();
                     return [4 /*yield*/, fs.readFile(filePath, "utf-8")];
                 case 4:
-                    file = _b.sent();
+                    file = _a.sent();
                     return [2 /*return*/, res.send(file)];
                 case 5:
-                    err_1 = _b.sent();
+                    err_1 = _a.sent();
                     return [2 /*return*/, res.send("\n          user-agent: *\n          disallow: /downloads/\n          disallow: /private/\n          allow: /\n          \n          user-agent: magicsearchbot\n          disallow: /uploads/\n        ")];
                 case 6:
                     // ! Sitemap Fix
@@ -110,7 +109,7 @@ export default function handler(req, res) {
                     filePath = join(appPath, "dist/client", segmentsWithoutOrigin.join("/"));
                     return [4 /*yield*/, fs.readFile(filePath, "utf-8")];
                 case 7:
-                    file = _b.sent();
+                    file = _a.sent();
                     if (url.endsWith(".js") || url.endsWith(".css")) {
                         return [2 /*return*/, new Response(file, {
                                 headers: {
@@ -123,10 +122,10 @@ export default function handler(req, res) {
                     }
                     return [4 /*yield*/, fs.readFile(filePath)];
                 case 8:
-                    otherFile = _b.sent();
+                    otherFile = _a.sent();
                     return [4 /*yield*/, fileTypeFromBuffer(otherFile)];
                 case 9:
-                    result = _b.sent();
+                    result = _a.sent();
                     mimeType = result
                         ? result.mime
                         : url.endsWith(".svg")
@@ -142,7 +141,7 @@ export default function handler(req, res) {
                     if (!(url.endsWith(".js") || url.endsWith(".css"))) return [3 /*break*/, 12];
                     return [4 /*yield*/, fs.readFile(url, "utf-8")];
                 case 11:
-                    file = _b.sent();
+                    file = _a.sent();
                     return [2 /*return*/, res
                             .status(200)
                             .setHeader("Content-Type", url.endsWith(".js") ? "text/javascript" : "text/css")
@@ -154,7 +153,7 @@ export default function handler(req, res) {
                     bootstrapDirPath = join(appPath, "dist/client/assets");
                     return [4 /*yield*/, import(serverFilePath)];
                 case 13:
-                    entry = _b.sent();
+                    entry = _a.sent();
                     bootstrap = "/assets/" +
                         fsSync
                             .readdirSync(bootstrapDirPath)
@@ -168,7 +167,7 @@ export default function handler(req, res) {
                     fetchRequest = createFetchRequest(req, host);
                     return [4 /*yield*/, handler_1.query(fetchRequest)];
                 case 14:
-                    context = _b.sent();
+                    context = _a.sent();
                     status_1 = context.status;
                     if (status_1 === 302) {
                         redirect = context.headers.get("Location");
@@ -177,29 +176,16 @@ export default function handler(req, res) {
                     }
                     helmetContext = {};
                     router = createStaticRouter(handler_1.dataRoutes, context);
-                    if (!config.experimental.stream) return [3 /*break*/, 16];
-                    return [4 /*yield*/, render(router, context, helmetContext, bootstrap, styles, res)];
-                case 15: return [2 /*return*/, _b.sent()];
-                case 16: return [4 /*yield*/, render(router, context, helmetContext)];
-                case 17:
-                    rendered = _b.sent();
-                    // Load template html
-                    if (!templateHtml) {
-                        templateHtml = loadTemplateHtml(helmetContext, bootstrap, styles);
-                    }
-                    html = templateHtml.replace("rasengan-body-app", (_a = rendered.html) !== null && _a !== void 0 ? _a : "");
-                    // Send the rendered html page
-                    return [2 /*return*/, res
-                            .status(200)
-                            .setHeader("Content-Type", "text/html")
-                            .setHeader("Cache-Control", "max-age=31536000")
-                            .end(html)];
-                case 18:
-                    e_1 = _b.sent();
+                    return [4 /*yield*/, render(router, context, helmetContext, bootstrap, styles, res, "vercel")];
+                case 15: 
+                // If stream mode enabled, render the page as a plain text
+                return [2 /*return*/, _a.sent()];
+                case 16:
+                    e_1 = _a.sent();
                     console.log(e_1.stack);
                     res.status(500).end(e_1.stack);
-                    return [3 /*break*/, 19];
-                case 19: return [2 /*return*/];
+                    return [3 /*break*/, 17];
+                case 17: return [2 /*return*/];
             }
         });
     });
